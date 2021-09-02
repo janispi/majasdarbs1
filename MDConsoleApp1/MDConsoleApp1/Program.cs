d6d6ab78de5f97055644e857f296cdf7b62915b8
janispi <janis.pirsko@gmail.com>
2021-09-02 09:43:46 +0300

    added console App

---
+++ MDConsoleApp1/MDConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MDConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
